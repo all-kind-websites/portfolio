aa558c6a19226473bd5ce5ffc4b1d1e503ae0e0b
foti <footios76@gmail.com>
2022-09-20 21:47:49 +0300

    Add fonts and assets

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
-
+import './index.css'
 function App() {
   return (
     <div className="App">
       <h1 className='text-4xl font-bold' >Hello</h1>
+      <h1 className='text-4xl font-signature ' >Hello</h1>
     </div>
   );
 }

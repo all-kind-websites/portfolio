aedf58aa6b185dd63ed060db1e4841a19b853cdc
foti <footios76@gmail.com>
2022-09-21 18:19:43 +0300

    Add Portfolio

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { About, Home, NavBar, SocialLinks } from './components';
+import { About, Home, NavBar, Portfolio, SocialLinks } from './components';
 import './index.css';
 
 
@@ -9,6 +9,7 @@ function App() {
       <NavBar />
       <Home />
       <About />
+      <Portfolio />
       <SocialLinks />
     </div>
   );
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,5 @@
 export { default as About } from "./About";
 export { default as Home } from "./Home";
+export { default as Portfolio } from "./Portfolio";
 export { default as NavBar } from "./NavBar";
 export { default as SocialLinks } from "./SocialLinks";

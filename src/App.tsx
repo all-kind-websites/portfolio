f73e33e0db771c5e28a5ea46fa617daae8064786
foti <footios76@gmail.com>
2022-09-21 18:52:10 +0300

    Add Experience

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { About, Home, NavBar, Portfolio, SocialLinks } from './components';
+import { About, Experience, Home, NavBar, Portfolio, SocialLinks } from './components';
 import './index.css';
 
 
@@ -10,6 +10,7 @@ function App() {
       <Home />
       <About />
       <Portfolio />
+      <Experience />
       <SocialLinks />
     </div>
   );

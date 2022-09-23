6e9f38bed490bda1dc8702b0dbb08e05d88e42ff
foti <footios76@gmail.com>
2022-09-23 03:07:01 +0300

    Add content in Experience

--- src/misc/techs.ts
+++ src/misc/techs.ts
@@ -1,67 +1,68 @@
-const html = require("../assets/html.png");
-const css = require("../assets/css.png");
-const javascript = require("../assets/javascript.png");
-const nextjs = require("../assets/nextjs.png");
-const graphql = require("../assets/graphql.png");
-const node = require("../assets/node.png");
-const reactImg = require("../assets/react.png");
-const tailwind = require("../assets/tailwind.png");
-// const github = require('../assets/github.png')
+const css = require("../assets/techs/css.png");
+const sass = require("../assets/techs/sass.png");
+const typescript = require("../assets/techs/typescript.png");
+const reactImg = require("../assets/techs/react.png");
+const firebase = require("../assets/techs/firebase.png");
+const redux = require("../assets/techs/redux.webp");
+const git = require("../assets/techs/git.png");
+const github = require("../assets/techs/github.png");
+const javascript = require("../assets/techs/javascript.png");
 
 const techs = [
   {
     id: 0,
-    src: html,
-    title: "HTML",
-    style: "shadow-orange-500",
-  },
-  {
-    id: 1,
     src: css,
-    title: "CSS",
+    title: "HTML/CSS",
     style: "shadow-blue-500",
   },
   {
+    id: 1,
+    src: sass,
+    title: "Sass",
+    style: "shadow-pink-400",
+  },
+  {
     id: 2,
-    src: javascript,
-    title: "JavaScript",
-    style: "shadow-yellow-500",
+    src: typescript,
+    title: "Typescript",
+    style: "shadow-blue-400",
   },
   {
     id: 3,
     src: reactImg,
-    title: "React",
+    title: "ReactNative",
     style: "shadow-blue-600",
   },
   {
     id: 4,
-    src: tailwind,
-    title: "Tailwind",
-    style: "shadow-sky-400",
+    src: firebase,
+    title: "Firebase",
+    style: "shadow-yellow-500",
   },
   {
     id: 5,
-    src: nextjs,
-    title: "Next JS",
-    style: "shadow-white",
+    src: git,
+    title: "Git",
+    style: "shadow-orange-800",
   },
+
   {
     id: 6,
-    src: graphql,
-    title: "GraphQL",
-    style: "shadow-pink-400",
-  },
-  {
-    id: 7,
-    src: node,
-    title: "Node JS",
-    style: "shadow-green-500",
+    src: redux,
+    title: "Redux",
+    style: "shadow-purple-500",
   },
   // {
-  //   id: 7,
+  //   id: 2,
+  //   src: javascript,
+  //   title: "JavaScript",
+  //   style: "shadow-yellow-500",
+  // },
+  // {
+  //   id: 8,
   //   src: github,
-  //   title: 'GitHub',
-  //   style: 'shadow-gray-400'
+  //   title: "GitHub",
+  //   style: "shadow-gray-400",
   // },
 ];
 

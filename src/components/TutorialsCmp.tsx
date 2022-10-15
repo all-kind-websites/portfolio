2453263df56f388a49356962e54e68df5491e0cf
foti <footios76@gmail.com>
2022-10-15 10:08:43 +0300

    Fix bug with tech logo in TutorialsList

--- src/components/TutorialsCmp.tsx
+++ src/components/TutorialsCmp.tsx
@@ -14,9 +14,9 @@ interface Props {
 const TutorialsCmp: FC<Props> = ({ tutorials, src, name, color, shadow, text }) => {
   return (
     <section
-      className={`flex flex-col justify-around items-center w-full ${tutorials.length > 3 ? 'h-full' : 'h-screen'}  bg-gradient-to-b from-black to-gray-800 overflow-auto scrollbar-hide pt-96 pb-4 md:pt-10 mx-auto px-16`}
+      className={`flex flex-col justify-around items-center w-full ${tutorials.length > 3 ? '' : 'h-screen'}  bg-gradient-to-b from-black to-gray-800 overflow-auto scrollbar-hide pt-96 pb-4 md:pt-10 mx-auto px-16`}
     >
-      <div className={`${tutorials.length > 3 ? 'mt-10' : ''}`} >
+      <div className={`${tutorials.length > 3 ? ' mt-10 ' : ''}`} >
         <TechLogo src={src} name={name} shadow={shadow} />
       </div>
       <TutorialsList tutorials={tutorials} color={color} text={text} />
